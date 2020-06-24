f86c0891cf5ff78bdf1b96e8af6880caaf44f799
Mrinal Sharma <sharma.mr@husky.neu.edu>
2020-06-24 15:36:08 -0700

    O(sqrt(n) * n) time complexity

---
+++ Math/CountPrimes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countPrimes(int n) {
+        
+        boolean primes[] = new boolean[n];
+        
+        Arrays.fill(primes,true);
+        
+        for(int i = 2; i*i < primes.length; i++) {
+            
+            if(primes[i]) {
+                
+                for(int j = i; j*i < primes.length; j++) {
+                    primes[i*j] = false;
+                }
+            }
+        }
+        
+        int primeCount = 0;
+        
+        for(int i = 2; i< primes.length; i++) {
+            
+            if(primes[i]) {
+                
+                primeCount++;
+            }
+        }
+        
+        return primeCount;
+    }
+}
