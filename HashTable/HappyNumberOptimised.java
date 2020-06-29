234cc95e86f05bd3b0f6ccb2dd73b3414959835b
Mrinal Sharma <sharma.mr@husky.neu.edu>
2020-06-29 12:36:51 -0700

    O(logN) time and O(1) space complexity

---
+++ HashTable/HappyNumberOptimised.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isHappy(int n) { 
+        
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while(fastRunner != 1 && fastRunner != slowRunner) {
+            
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        
+        return fastRunner == 1;
+    }
+    
+    
+    public int getNext(int n) {
+          int sum = 0;  
+        while(n > 0) {
+            
+            int d = n%10;
+            n = n/10;
+            sum += d*d;
+        }
+        
+        return sum;
+        
+    }
+}
